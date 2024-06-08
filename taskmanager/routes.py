1a12dcab39cf8159cf21b8b6fc14dd9291557132
Sebastian Bucher <Sebasti.b@web.de>
2024-06-08 14:14:59 +0000

    fix: remove unnecessary commas

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -63,10 +63,10 @@ def edit_task(task_id):
     task = Task.query.get_or_404(task_id)
     categories = list(Category.query.order_by(Category.category_name).all())
     if request.method == "POST":
-        task.task_name = request.form.get("task_name"),
-        task.task_description = request.form.get("task_description"),
-        task.is_urgent = bool(True if request.form.get("is_urgent") else False),
-        task.due_date = request.form.get("due_date"),
+        task.task_name = request.form.get("task_name")
+        task.task_description = request.form.get("task_description")
+        task.is_urgent = bool(True if request.form.get("is_urgent") else False)
+        task.due_date = request.form.get("due_date")
         task.category_id = request.form.get("category_id")
         db.session.commit()
     return render_template("edit_task.html", task=task, categories=categories)
