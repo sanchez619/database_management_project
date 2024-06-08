c7699eb082bfbd67ed39aaabbd7734be1902c5ce
Sebastian Bucher <Sebasti.b@web.de>
2024-06-08 13:03:15 +0000

    fix: compare and add source code where necessary

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,14 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var sidnav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
 
-    // datepicker initialization
-    let datepicker = document.querySelectorAll('.datepicker');
-    M.Datepicker.init(datepicker,{
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
       format: "dd mmmm, yyyy",
-      i18n: {done: "Select"},
-    });
-    // select initialization
-    let selects = document.querySelectorAll('select');
-    M.FormSelect.init(selects);
-  });    
+      i18n: {done: "Select"}
+  });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+});
